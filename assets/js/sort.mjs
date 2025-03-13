bc0e6fa864552c110b64d2dbada8fb7a93c7defc
GEdO23 <rm99632@fiap.com.br>
2025-03-13 17:58:53 -0300

    feat: adicionar função de ordenação e testes com Cypress
    - Criação da função `sort.mjs` para ordenar uma lista de números.
    - Adição de testes automatizados com Cypress no arquivo `sort.cy.js`, validando diferentes cenários, como listas vazias e já ordenadas.

---
+++ assets/js/sort.mjs
@@ -0,0 +1,28 @@
+/**
+ * @file Ordenando números sem sort
+ * @author Gabriel Eringer de Oliveira <gabriel.eringer.23@gmail.com>
+ * @module blog/desafios/sort
+ */
+
+//Desafio Extra: Tente fazer uma função sem usar o sort para ordenar números
+
+/**
+ * 
+ * @param {number[]} numbers Um Array de números
+ * @returns {number[]} O Array de números ordenado em ordem crescente
+ */
+export function sortNumbers(numbers) {
+    for (let i = 0; i < numbers.length; i++)
+        for (let j = 0; j < numbers.length; j++)
+            if (numbers[j] > numbers[j + 1]) {
+                let temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+    return numbers
+}
+
+const numList = [6, 3, 4, 1, 8, 9, 2]
+console.log('Original:', numList)
+console.log('Ordenado com sort():', numList.sort())
+console.log('Ordenado com sortNumbers():', sortNumbers(numList))
