30bd4f47992865ac713649f98d81198bde393565
GEdO23 <rm99632@fiap.com.br>
2025-04-28 13:57:51 -0300

    feat(gerenciamento-usuarios-escola): listar e filtrar usuários com AngularJS

--- gerenciamento-usuarios-escola/assets/js/script.js
+++ gerenciamento-usuarios-escola/assets/js/script.js
@@ -4,11 +4,37 @@ const app = angular.module('app', []);
 
 app.controller('AppController', function ($scope) {
     $scope.greetings = 'Bem-vindo ao sistema de cadastro escolar';
-
-    $scope.usuario = {
-        nome: 'João Alvares',
-        tipo: 'Professor'
-    };
-
-    $scope.isProfessor = $scope.usuario.tipo == 'Professor';
 });
+
+app.controller('ListaDeAlunosController', function ($scope) {
+    $scope.filtro = '';
+    $scope.filtroTipo = '';
+
+    $scope.usuarios = [
+        {
+            nome: 'João Alvares',
+            tipo: 'Professor',
+            dataCadastro: new Date()
+        },
+        {
+            nome: 'Pedro Silva',
+            tipo: 'Aluno',
+            dataCadastro: new Date()
+        },
+        {
+            nome: 'Renata dos Campos',
+            tipo: 'Aluno',
+            dataCadastro: new Date().setMonth('05')
+        },
+        {
+            nome: 'Maurício Oliveira',
+            tipo: 'Professor',
+            dataCadastro: new Date()
+        },
+        {
+            nome: 'Maria Isabela',
+            tipo: 'Aluno',
+            dataCadastro: new Date()
+        }
+    ]
+})
