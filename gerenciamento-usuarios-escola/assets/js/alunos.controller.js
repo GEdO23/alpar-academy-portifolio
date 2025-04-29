e53f219ed5fccc27f67e4755b65c80f7d67ec5f7
GEdO23 <rm99632@fiap.com.br>
2025-04-29 18:42:23 -0300

    feat(gerenciamento-usuarios-escola): atualizar estilo da página

--- gerenciamento-usuarios-escola/assets/js/alunos.controller.js
+++ gerenciamento-usuarios-escola/assets/js/alunos.controller.js
@@ -5,28 +5,10 @@ window.app.controller('ListaDeAlunosController', function ($scope, UsuarioServic
     $scope.filtro = '';
     $scope.filtroTipo = '';
     $scope.usuarios = UsuarioService.listar();
-    $scope.usuarioInput = {
-        nome: '',
-        tipo: ''
-    }
+    $scope.isFilterListVisible = true;
 
-    $scope.toggleModal = () => {
-        $scope.isModalActive = !$scope.isModalActive;
-    };
-
-    $scope.adicionarUsuario = () => {
-        const nome = $scope.usuarioInput.nome;
-        const tipo = $scope.usuarioInput.tipo;
-        if (!nome || !tipo) {
-            throw new Error("Novo usuário precisa obrigatoriamente ter um nome e um tipo");
-        }
-
-        UsuarioService.adicionar({ nome, tipo, dataCadastro: new Date() });
-
-        $scope.usuarioInput.nome = '';
-        $scope.usuarioInput.tipo = '';
-
-        $scope.usuarios = UsuarioService.listar();
+    $scope.toggleFilterList = () => {
+        $scope.isFilterListVisible = !$scope.isFilterListVisible;
     }
 
     $scope.removerUsuario = (usuario) => {
--- gerenciamento-usuarios-escola/assets/js/app.controller.js
+++ gerenciamento-usuarios-escola/assets/js/app.controller.js
@@ -1,5 +1,42 @@
 // @ts-nocheck
 
-window.app.controller('AppController', function ($scope) {
+window.app.controller('AppController', function ($scope, UsuarioService) {
     $scope.greetings = 'Bem-vindo ao sistema de cadastro escolar';
+    $scope.isGreetingsVisible = true;
+
+    $scope.closeGreetings = () => {
+        $scope.isGreetingsVisible = false;
+    };
+
+    $scope.usuario = {
+        nome: 'Gabriel Eringer',
+        tipo: 'Professor'
+    };
+
+    $scope.isProfessor = $scope.usuario.tipo == 'Professor';
+
+    $scope.usuarioInput = {
+        nome: '',
+        tipo: ''
+    }
+
+    $scope.toggleModal = () => {
+        $scope.isModalActive = !$scope.isModalActive;
+    };
+
+    $scope.adicionarUsuario = () => {
+        const nome = $scope.usuarioInput.nome;
+        const tipo = $scope.usuarioInput.tipo;
+        if (!nome || !tipo) {
+            throw new Error("Novo usuário precisa obrigatoriamente ter um nome e um tipo");
+        }
+
+        UsuarioService.adicionar({ nome, tipo, dataCadastro: new Date() });
+
+        $scope.usuarioInput.nome = '';
+        $scope.usuarioInput.tipo = '';
+
+        $scope.usuarios = UsuarioService.listar();
+        $scope.toggleModal();
+    }
 });
